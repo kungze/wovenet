b612c83506e2fdde4b63810f0f2bf8a8a76e2ba7
JeffYang <yangjianfeng@troila.com>
2025-04-18 14:39:47 +0800

    check config options and set default values

--- internal/message/config.go
+++ internal/message/config.go
@@ -1,7 +1,39 @@
 package message
 
+import (
+	"fmt"
+	"net/url"
+	"slices"
+	"strings"
+)
+
 type Config struct {
 	Protocol  string     `mapstructure:"protocol"`
 	CryptoKey string     `mapstructure:"cryptoKey"`
 	Mqtt      mqttConfig `mapstructure:"mqtt"`
+}
+
+func CheckAndSetDefaultConfig(config Config) (*Config, error) {
+	if !slices.Contains([]string{MQTT}, strings.ToLower(config.Protocol)) {
+		return nil, fmt.Errorf("unsupported message protocol: %s", config.Protocol)
+	}
+	if len(config.CryptoKey) != 16 {
+		return nil, fmt.Errorf("the expected length of message cryptoKey is 16, but got lenght: %d", len(config.CryptoKey))
+	}
+	if config.Protocol == strings.ToLower(MQTT) {
+		if config.Mqtt.Topic == "" {
+			return nil, fmt.Errorf("the mqtt topic must be set")
+		}
+		if config.Mqtt.BrokerServer == "" {
+			config.Mqtt.BrokerServer = "mqtt://mqtt.eclipseprojects.io:1883"
+		}
+		if !strings.HasPrefix(config.Mqtt.BrokerServer, "mqtt://") {
+			return nil, fmt.Errorf("the mqtt brokerServer must start with 'mqtt://'")
+		}
+		_, err := url.Parse(config.Mqtt.BrokerServer)
+		if err != nil {
+			return nil, fmt.Errorf("the brokerServer: %s is not a valid url", config.Mqtt.BrokerServer)
+		}
+	}
+	return &config, nil
 }
--- internal/tunnel/config.go
+++ internal/tunnel/config.go
@@ -1,5 +1,11 @@
 package tunnel
 
+import (
+	"fmt"
+	"net"
+	"slices"
+)
+
 type SocketConfig struct {
 	Mode              SocketMode        `mapstructure:"mode"`
 	TransportProtocol TransportProtocol `mapstructure:"transportProtocol"`
@@ -11,4 +17,45 @@ type SocketConfig struct {
 
 type Config struct {
 	LocalSockets []SocketConfig `mapstructure:"localSockets"`
+}
+
+func CheckAndSetDefaultConfig(config Config) (*Config, error) {
+	for _, socket := range config.LocalSockets {
+		if !slices.Contains([]SocketMode{PortForwarding, DedicatedAddress}, socket.Mode) {
+			return nil, fmt.Errorf("unsupported tunnel socket mode: %s", socket.Mode)
+		}
+		if !slices.Contains([]TransportProtocol{QUIC}, socket.TransportProtocol) {
+			return nil, fmt.Errorf("unsupported tunnel transport protocol: %s", socket.TransportProtocol)
+		}
+		if socket.Mode == DedicatedAddress {
+			if socket.PublicAddress == "" && socket.ListenAddress == "" {
+				return nil, fmt.Errorf("the 'publicAddress' or 'listenAddress' must be set when the tunnel socket mode set as: %s", DedicatedAddress)
+			} else if socket.PublicAddress == "" {
+				socket.PublicAddress = socket.ListenAddress
+			}
+			if socket.ListenPort == 0 && socket.PublicePort == 0 {
+				return nil, fmt.Errorf("the 'publicPort' or 'listenPort' must be set when the tunnel socket mode set as: %s", DedicatedAddress)
+			} else if socket.PublicePort == 0 {
+				socket.PublicePort = socket.ListenPort
+			} else if socket.ListenPort == 0 {
+				socket.ListenPort = socket.PublicePort
+			}
+			if socket.ListenPort != socket.PublicePort {
+				return nil, fmt.Errorf("the 'publicPort' or 'listenPort' must be equal when the tunnel socket mode set as: %s", DedicatedAddress)
+			}
+		}
+		if socket.Mode == PortForwarding && (socket.PublicAddress == "" || socket.PublicePort == 0 || socket.ListenPort == 0) {
+			return nil, fmt.Errorf("the 'publicAddress' and 'publicPort' and 'listenPort' must be set together when the tunnel socket mode set as: %s", PortForwarding)
+		}
+		if socket.ListenAddress == "" {
+			socket.ListenAddress = "0.0.0.0"
+		}
+		if net.ParseIP(socket.PublicAddress) == nil {
+			return nil, fmt.Errorf("the 'publicAddress' is invalid")
+		}
+		if net.ParseIP(socket.ListenAddress) == nil {
+			return nil, fmt.Errorf("the 'listenAddress' is invalid")
+		}
+	}
+	return &config, nil
 }
